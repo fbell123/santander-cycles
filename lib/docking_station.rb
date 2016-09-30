5d6f23bceb9dda1ee0be65ef7283bd452faa1b78
Frankie Bell <fbell.pro@gmail.com>
2016-09-30 11:22:56 +0100

    step 15, created full and empty methods, refactored to Single Responsibility

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -10,16 +10,32 @@ class DockingStation
 	attr_reader :bikes
 
 	def release_bike
-			fail "No bikes available" unless @bikes.count >= 1
+			fail "No bikes available" if empty?
 			@bikes
 	end
 
 	def dock(bike)
-			fail "Dock is full" if @bikes.count == 20
+			fail "Dock is full" if full?
 			@bikes << bike
 	end
 
+private
 
+	def full?
+		if @bikes.count == 20
+			true
+		else
+			false
+		end
+	end
+
+	def empty?
+		if @bikes.count == 0
+			true
+		else
+			false
+		end
+	end
 
 
 end
